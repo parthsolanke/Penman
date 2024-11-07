d13d29ba29bd68caab38c8d4254966f2a1a34559
parthsolanke <parthsolanke2002@gmail.com>
2024-11-07 14:15:34 +0000

    feat: add dockerfile; chore: update requirements.txt; chore: add .env template

--- backend/main.py
+++ backend/main.py
@@ -2,22 +2,32 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router as handwriting_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+environment = os.getenv("ENVIRONMENT")
+if environment == "production":
+    allow_origins = os.getenv("PROD_ALLOWED_ORIGINS").split(",")
+    allow_methods = os.getenv("PROD_ALLOWED_METHODS").split(",")
+    allow_headers = os.getenv("PROD_ALLOWED_HEADERS").split(",")
+else:
+    allow_origins = os.getenv("DEV_ALLOWED_ORIGINS").split(",")
+    allow_methods = os.getenv("DEV_ALLOWED_METHODS").split(",")
+    allow_headers = os.getenv("DEV_ALLOWED_HEADERS").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allow_origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=allow_methods,
+    allow_headers=allow_headers,
 )
 
 app.include_router(handwriting_router, prefix="/handwriting")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
